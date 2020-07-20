79574cc1778a89d843a92312746ce7e1babb5cf3
nayanashettyft <nayana.shetty@ft.com>
2020-07-20 13:30:47 +0100

    get app working with next-metrics

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
+const members = require('./Members');
+const metrics = require('next-metrics');
+
+metrics.init({
+  flushEvery: 5000
+});
 
 const app = express();
 
 //  Init Middleware
 // app.use(logger);
 
-// Body Parser Middleware
+// Body Parser Middleware for parsing HTTP post response body
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
+// Express handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req,res) => {
+  metrics.instrument(res, { as: 'express.http.res' });
+  metrics.count('long.count.hellojshdsjdhjskcns.sdcdscdscds.vdscsdcscf.afefaewfawef.adcfadscfaedf.faedfcadfaerwf.faedfaerferrae.ferfersfserfgse.ferfaerferwf.faerfwerfweferrfhserfj.fesrfserf.frefgerferg.tfyjftygg.sdfsdfvc.dwaedcs')
+  return res.render('index', {
+    title: 'Member App',
+    members
+  });
+});
 
 // Setup static folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -18,4 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use('/api/members', require('./routers/api/members'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
